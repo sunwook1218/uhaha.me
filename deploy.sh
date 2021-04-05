2e6a34372376c8fb15e255783554f0ec59e48f6a
sunwook1218 <65208060+sunwook1218@users.noreply.github.com>
2021-04-05 17:18:04 +0900

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+REPOSITORY=/home/ec2-user/app
+PROJECT_NAME=uhaha.me
+
+cd $REPOSITORY/$PROJECT_NAME
+
+echo "> Git Pull"
+
+git pull
+
+echo "> 프로젝트 빌드 시작"
+
+./gradlew build
+
+echo "> 상위 디렉토리로 이동"
+
+cd $REPOSITORY
+
+echo "> Build 파일 복사"
+
+cp $REPOSITORY/$PROJECT_NAME/build/libs/*.jar $REPOSITORY/
+
+echo "> 현재 구동중인 애플리케이션 pid 확인"
+
+JAR_NAME=$(ls -tr $REPOSITORY | grep jar | tail -n 1)
+
+CURRENT_PID=$(ps -ef | grep $JAR_NAME | grep -v grep | tail -n 1)
+
+echo "> 현재 구동중인 애플리케이션 pid: $CURRENT_PID"
+
+
+if [ -z "$CURRENT_PID" ]; then
+	echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else 
+	echo "> kill -15 $CURRENT_PID"
+	kill -15 $CURRENT_PID
+	sleep 3
+fi
+
+echo "> 새 애플리케이션 배포"
+
+echo "> JAR Name: $JAR_NAME"
+
+nohup java -jar $REPOSITORY/$JAR_NAME 2>&1 &
